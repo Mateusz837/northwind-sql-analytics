bbcf644bee4cbb2433e74d340ea1fa066a74cff0
Mateusz837 <mateusz.kozlinski@gmail.com>
2025-12-21 18:35:47 +0100

    Update 02_temporal_analysis.sql

--- sql/02_temporal_analysis.sql
+++ sql/02_temporal_analysis.sql
@@ -4,6 +4,29 @@
 
 -- Task 1: Top 3 products by revenue per country
 
+WITH product_revenue_by_country AS (
+    SELECT
+        c.country, p.productName,
+        SUM((od.unitPrice * od.quantity) * (1 - od.discount)) AS total_revenue
+    FROM salesorder s
+    LEFT JOIN customer c ON s.custId = c.custId
+    LEFT JOIN orderdetail od ON s.orderId = od.orderId
+    LEFT JOIN product p ON od.productId = p.productId
+    GROUP BY c.country, p.productName
+),
+ranked_products AS (
+    SELECT
+        country, productName, total_revenue,
+        DENSE_RANK() OVER (PARTITION BY country ORDER BY total_revenue DESC) AS product_rank
+    FROM product_revenue_by_country
+)
+SELECT
+    country, productName, total_revenue, product_rank
+FROM ranked_products
+WHERE product_rank <= 3
+ORDER BY country, product_rank;
+
+
 
 -- Task 2: Month-over-month revenue change
 
