39aeca33616a2b19f5112e50202a9fcca1976f02
Mateusz837 <mateusz.kozlinski@gmail.com>
2025-12-23 19:01:40 +0100

    Update 05_date_dimension.sql

--- sql/05_date_dimension.sql
+++ sql/05_date_dimension.sql
@@ -1,1 +1,16 @@
+-- =========================================================
+-- Module 5: Date Dimension & Daily Activity
+-- =========================================================
 
+-- Task 1: Create dim_date (date table without gaps)
+
+
+
+
+
+
+
+-- Task 2: Days without orders (daily activity)
+
+
+-- Task 3: Days without sales per month
